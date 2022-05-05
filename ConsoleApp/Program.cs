2b4d9d9db313fa4a27b77395e9f48812131edb06
Jairo Andres Wilches <andresing.81.17@gmail.com>
2022-05-05 09:04:50 -0500

    subir cambios

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-us");
+            
+            Smoothie f1 = new Smoothie(new string[] { "Raspberries", "Strawberries", "Blueberries" });
+            Console.WriteLine("Smoothie #2");
+            Console.Write("Smoothie Ingredients: ");
+            Console.WriteLine("{{ {0} }}", string.Join(", ", f1.Ingredients));
+            Console.Write("Smoothie Cost: ");
+            Console.WriteLine("{0}", f1.GetCost());
+            Console.Write("Smoothie Price: ");
+            Console.WriteLine(f1.GetPrice());
+            Console.Write("Smoothie Name: ");
+            Console.WriteLine(f1.GetName());
+
+
+            //Console.WriteLine("Hello World");
+
+            //Tests test = new Tests();
+            //test.Test1();
+
+
+        }
+    }
+}
---
+++ ConsoleApp/Smoothie.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Smoothie
+    {
+        public Smoothie (string[] ingredients)
+        {
+            this.ingredients = ingredients;
+        }
+
+        static Dictionary<string, string> prices = new Dictionary<string, string>()
+        {
+            { "Strawberries", "$1.50" },
+            { "Banana", "$0.50" },
+            { "Mango", "$2.50" },
+            { "Blueberries", "$1.00" },
+            { "Raspberries", "$1.00" },
+            { "Apple", "$1.75" },
+            { "Pineapple", "$3.50" }          
+
+        };
+        private readonly string[] ingredients;
+
+        public string[] Ingredients 
+        {           
+            get { return this.ingredients; }
+            set { this.Ingredients = value; }    
+            
+        }
+
+        public string GetCost()
+        {
+            double coste = 0;
+            foreach (string ingredient in this.ingredients)
+            {
+                if (Smoothie.prices.ContainsKey(ingredient))
+                {
+                    
+                    var valueDict = Smoothie.prices[ingredient];                    
+                    var removerSing = valueDict.Replace("$", "");
+                    var valuePrice = double.Parse(removerSing);
+
+                    coste += valuePrice;
+                }
+            }
+            return coste.ToString("C", CultureInfo.CurrentCulture);
+        }
+
+        public string GetPrice()
+        {
+            var valuePrice = this.GetCost();
+            var removerSing = valuePrice.Replace("$", "");
+            var getPrice = Math.Round((double.Parse(removerSing) * 2.5),2).ToString("C", CultureInfo.CurrentCulture);
+            return getPrice;
+        }
+
+        public string GetName()
+        {
+            List<string> names = new List<string>();
+            foreach (var item in this.ingredients)
+            {
+                if (item.Length > 9)
+                {
+                    if (item.Substring(item.Length - 7) == "berries")
+                    {
+                        names.Add(item.Replace( "ies","y"));
+                    }
+                    else
+                    {
+                        names.Add(item);
+                    }
+                }
+                else 
+                {
+                    names.Add(item);
+                }
+            }
+
+            if (names.Count < 2)
+            {
+                names.Add("Smoothie");
+
+            }
+            else
+            {
+                names.Sort();
+                names.Add("Fusion");
+            }
+            return string.Join(" ",names);
+        }
+
+    }
+}
